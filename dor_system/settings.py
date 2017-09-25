e2691c5818d3395bbfbbd5930e71a27119aadd21
张敏华 <1578711560@qq.com>
2017-09-26 03:14:10 +0800

    加载js、css

--- dor_system/settings.py
+++ dor_system/settings.py
@@ -118,12 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = '/home/dormitory_manage_system/venv/bin/dor_system/'
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    ('css',os.path.join(STATIC_ROOT,'css').replace('\\','/') ),
-    ('js',os.path.join(STATIC_ROOT,'js').replace('\\','/') ),
+    os.path.join(BASE_DIR, 'static'),
 )
