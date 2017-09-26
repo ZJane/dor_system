4b8d4e3ce14723b46a73703c2106514e1d61522d
张敏华 <1578711560@qq.com>
2017-09-26 14:12:33 +0800

    modified settings.py

--- dor_system/settings.py
+++ dor_system/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = '/home/dormitory_manage_system/venv/bin/dor_system/'
-STATIC_URL = '/static/'
+STATIC_URL = '/home/dormitory_manage_system/venv/bin/dor_system/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
